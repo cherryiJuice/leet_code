42dfbd832798edc5a759a0a4ed6c1dd1ee8cafe3
cherryiJuice <143402486+cherryiJuice@users.noreply.github.com>
2026-04-26 13:18:01 +0900

    Time: 1 ms (85.86%), Space: 45.3 MB (79.55%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,20 +1,20 @@
 import java.util.*;
-
 class Solution {
-    List<List<Integer>> answer = new ArrayList<>();
+    List<List<Integer>> result;
     boolean[] visited;
-
     public List<List<Integer>> permute(int[] nums) {
+        result = new ArrayList<>();
         visited = new boolean[nums.length];
+        
         dfs(nums, new ArrayList<>());
-        return answer;
+        return result;
     }
 
-    private void dfs(int[] nums, List<Integer> temp) {
+    void dfs(int[] nums, List<Integer> temp) {
         if(temp.size() == nums.length) {
-            answer.add(new ArrayList<>(temp));
-            return;
+            result.add(new ArrayList<>(temp));
         }
+
         for(int i=0; i<nums.length; i++) {
             if(!visited[i]) {
                 temp.add(nums[i]);
